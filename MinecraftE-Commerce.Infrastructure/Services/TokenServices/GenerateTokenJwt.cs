048c56d5d599a7608ae89a4167959184442c2647
syzenzzw <oisyzenx@gmail.com>
2025-02-22 13:18:47 -0300

    Add the token service

--- MinecraftE-Commerce.Infrastructure/Services/TokenServices/GenerateTokenJwt.cs
+++ MinecraftE-Commerce.Infrastructure/Services/TokenServices/GenerateTokenJwt.cs
@@ -1,9 +1,47 @@
-﻿using MinecraftE_Commerce.Domain.Interfaces;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using MinecraftE_Commerce.Domain.Interfaces;
+using MinecraftE_Commerce.Domain.Models;
 
 namespace MinecraftE_Commerce.Infrastructure.Services.TokenServices
 {
     public class GenerateTokenJwt : ITokenService
     {
+        private readonly IConfiguration _configuration;
 
+        public GenerateTokenJwt(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        
+        public string CreateToken(User userModel)
+        {
+            var key = Encoding.UTF8.GetBytes(_configuration["JWT:Key"]!);
+
+            var clains = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Email, userModel.Email!),
+                new Claim(JwtRegisteredClaimNames.Name, userModel.UserName!)
+            };
+
+            var signInCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature);
+
+            var configToken = new SecurityTokenDescriptor
+            {
+                SigningCredentials = signInCredentials,
+                Expires = DateTime.UtcNow.AddDays(3),
+                Subject = new ClaimsIdentity(clains),
+                Issuer = _configuration["JWT:Issuer"],
+                Audience = _configuration["JWT:Audience"]
+            };
+
+            var handler = new JwtSecurityTokenHandler();
+            var token = handler.CreateToken(configToken);
+
+            return handler.WriteToken(token);
+        }
     }
 }
--- MinecraftE-Commerce/Program.cs
+++ MinecraftE-Commerce/Program.cs
@@ -1,5 +1,10 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using MinecraftE_Commerce.Domain.Interfaces;
+using MinecraftE_Commerce.Domain.Models;
 using MinecraftE_Commerce.Infrastructure.Data;
 using MinecraftE_Commerce.Infrastructure.Repositories;
 using MinecraftE_Commerce.Infrastructure.Services.TokenServices;
@@ -15,6 +20,43 @@ builder.Services.AddScoped<ITokenService, GenerateTokenJwt>();
 
 var ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
+builder.Services.AddIdentity<User, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequireNonAlphanumeric = true;
+}).AddEntityFrameworkStores<AppDbContext>();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme =
+    options.DefaultChallengeScheme =
+    options.DefaultForbidScheme =
+    options.DefaultSignOutScheme =
+    options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    var issuer = builder.Configuration["JWT:Issuer"];
+    var audience = builder.Configuration["JWT:Audience"];
+    var key = builder.Configuration["JWT: Key"];
+
+    if (issuer == null || audience == null || key == null)
+    {
+        throw new ArgumentException("Failed in .json file");
+    }
+
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        RequireExpirationTime = true,
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = issuer,
+        ValidAudience = audience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key))
+    };
+});
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
