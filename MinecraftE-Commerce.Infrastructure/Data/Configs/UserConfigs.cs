57ef86e5935768a5929da95d63cd8678245c2a92
syzenzzw <oisyzenx@gmail.com>
2025-02-22 10:28:50 -0300

    setup the config for announcement

---
+++ MinecraftE-Commerce.Infrastructure/Data/Configs/UserConfigs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinecraftE_Commerce.Infrastructure.Data.Configs
+{
+    internal class UserConfigs
+    {
+    }
+}
