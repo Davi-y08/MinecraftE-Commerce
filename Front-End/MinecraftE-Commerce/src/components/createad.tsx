cfa2dea6de8b80c775fa1fdc5aea0e3733cd0437
syzenzzw <oisyzenx@gmail.com>
2025-03-22 12:42:58 -0300

    fixed function not work

--- Front-End/MinecraftE-Commerce/src/components/createad.tsx
+++ Front-End/MinecraftE-Commerce/src/components/createad.tsx
@@ -1,65 +1,111 @@
 import { useState } from "react";
+import { useMutation } from "react-query";
+import { data } from "react-router-dom";
 
-function CreateAnnouncementPage(){
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-    const [price, setPrice] = useState('');
-    const [file, setFile] = useState<File | null>(null);
-    const bearer = "Bearer " + localStorage.getItem('token');
-    
-    const onFileChange = async (e: any) => {
-        setFile(e.target.files[0]);
+function CreateAnnouncementPage() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [file, setFile] = useState(null);
+  const bearer = "Bearer " + localStorage.getItem("token");
+  const [btnState, setBtnState] = useState('Enviar');
+
+  const onFileChange = async (e: any) => {
+    setFile(e.target.files[0]);
+  };
+
+  async function createAdAsync(formData: FormData) {
+    if (
+      !(title == null && description == null && price == null && file == null)
+    ) {
+      const response = await fetch("https://localhost:7253/api/v1/CreateAdd", {
+        method: "POST",
+        headers: {
+          Authorization: bearer,
+        },
+        body: formData,
+      });
+
+    } else {
+      alert("Preeencha todos os campos por favor!");
+    }
+  }
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!title || !description || !price || !file) {
+      alert("Por favor, preencha todos os campos.");
+      return;
     }
 
     const formData = new FormData();
-    formData.append("image", file);
-    formData.append("title", title);
-    formData.append("description", description);
-    formData.append("price", price);
+    formData.append("Title", title);
+    formData.append("Description", description);
+    formData.append("ImageAnnouncement", file);
+    formData.append("PriceService", price);
+    mutate(formData);
+  };
 
-    async function createAdAsync() {
-        if (!(title == null && description == null && price == null && file == null)) {
-            const response = await fetch('https://localhost:7253/api/v1/CreateAdd', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                    'Authorization': bearer
-                },
-                body: JSON.stringify({
-                    
-                })
-            })
-        }
-        else{
-            alert('erro na criação');
-        }
-    }
+  const {mutate, isLoading} = useMutation(createAdAsync, {
+    onSuccess: (data) => {
+        console.log(data);
+    } 
+  })
 
-    return(
-        <div>
-            <label htmlFor="titleforcreate">Title for announcemenet: </label>
-            <input type="text" name='titleforcreate' placeholder="title" onChange={(e) => setTitle(e.target.value)}/>
 
-            <br /><br />
+  if (isLoading) {
+    setBtnState('Carregando');
+  }
 
-            <label htmlFor="descriptionforcreate">Description for your ad: </label>
-            <input type="text" name="descriptionforcreate" placeholder="Description" onChange={(e) => setDescription(e.target.value)}/>
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="titleforcreate">Title for announcemenet: </label>
+        <input
+          type="text"
+          name="titleforcreate"
+          placeholder="title"
+          onChange={(e) => setTitle(e.target.value)}
+        />
 
-            <br /><br />
+        <br />
+        <br />
 
-            <label htmlFor="imageforcreate">Image for your announcement: </label>
-            <input type="file" name="imageforcreate" onChange={onFileChange}/>
+        <label htmlFor="descriptionforcreate">Description for your ad: </label>
+        <input
+          type="text"
+          name="descriptionforcreate"
+          placeholder="Description"
+          onChange={(e) => setDescription(e.target.value)}
+        />
 
-            <br /><br />
+        <br />
+        <br />
 
-            <label htmlFor="priceforcreate">Select your price ad: </label>
-            <input type="number" name="priceforcreate" onChange={(e) => setPrice(e.target.value)} placeholder="Price"/>
+        <label htmlFor="imageforcreate">Image for your announcement: </label>
+        <input type="file" name="imageforcreate" onChange={onFileChange} />
 
-            <br /><br />
+        <br />
+        <br />
 
-            <button type="button">Create</button>
-        </div>
-    );
+        <label htmlFor="priceforcreate">Select your price ad: </label>
+        <input
+          type="number"
+          name="priceforcreate"
+          onChange={(e) => setPrice(e.target.value)}
+          placeholder="Price"
+        />
+
+        <br />
+        <br />
+
+        <button type="submit" value={"Submit"}>
+          {btnState}
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default CreateAnnouncementPage;
